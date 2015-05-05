e43aa8d9baa6387852f6064e3e95e9c5d0baeba3
Tim Brandin <tim.brandin@gmail.com>
2015-05-05 13:23:04 +0200

    Testing without symlinks.

--- package.js
+++ package.js
@@ -27,14 +27,15 @@ function configurePackage(api) {
 
   api.addFiles('lib/fragment.html', 'client');
   api.addFiles([
-    'lib/layout.html',
-    'lib/layout.js'
+    'lib/boilerplate.html',
+    'lib/boilerplate.js'
   ], 'server', {isAsset: true});
 
   api.addFiles([
     'lib/overrides.js',
+    'lib/layout.js',
+    'lib/markdown.js',
     'lib/boot_templates.js',
     'lib/routes.js',
-    'lib/markdown.js',
   ], 'server');
 }
